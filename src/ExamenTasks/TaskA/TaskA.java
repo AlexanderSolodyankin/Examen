85a66219e9e4963a3a84d63bc862e4faf0d336dc
Alexander <89515014507@mail.ru>
2021-05-21 05:02:45 +0600

    Task B Complite

--- src/ExamenTasks/TaskA/TaskA.java
+++ src/ExamenTasks/TaskA/TaskA.java
@@ -1,11 +1,14 @@
 package ExamenTasks.TaskA;
 
+import java.util.Random;
+
 public class TaskA {
     public static void main(String[] args) {
+        Random ran = new Random();
         Circle[] cirls = new Circle[10];
         for (int i = 0; i < cirls.length; i++) {
-            cirls[i] = new Circle(i * 25);
-            System.out.println("Круг: " + i + " " + cirls[i]);
+            cirls[i] = new Circle(ran.nextInt(10 - 1 + 1) + 1);
+            System.out.println("Круг: " + (i + 1) + " " + cirls[i]);
 
         }
 
